db9b1e6ba56f32e1afb0b1a44331e64f17746b6e
unknown <nayem@aspyrer.com>
2021-12-29 10:30:19 +0600

    after installing react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import "./App.css";
+
+import DonutSourceRevenue from "./components/DonutSourceRevenue";
+
+function App() {
+  return (
+    <div className="App">
+      <div>jfdkfj</div>
+      <DonutSourceRevenue />
+    </div>
+  );
+}
+
+export default App;
